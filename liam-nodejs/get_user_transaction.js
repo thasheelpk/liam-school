a63385aff0223df9bfd500d4522f0b1286ee7cae
Mohammed Thasheel P K <thasheelpk@gmail.com>
2020-03-31 15:26:23 +0530

    transc

---
+++ liam-nodejs/get_user_transaction.js
@@ -0,0 +1,6 @@
+module.exports = function(req,res) {
+	var username = req.session.currUser;
+	require('./get_document_by_username.js')(username).then(function(doc) {
+		res.send(doc.transactions);
+	});
+}
